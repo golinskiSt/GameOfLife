6755abc60d334b43a7dd3afeceb1c5819a46db78
Stanisław Goliński <golinski.st2gmail.com>
2019-08-01 20:38:45 +0200

    bugfix 1.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function generateBoard() {
   for (let i = 0; i < 25; i++) {
     for (let j = 0; j < 25; j++) {
       var td = document.createElement("DIV");
-      
+
       td.onmousedown = function() {
         makeAlive(i, j);
       };
@@ -27,7 +27,7 @@ function generateBoard() {
       td.style.width = "20px";
       td.classList.add("float-left");
       if (board[i][j].isAlive) {
-        td.style.backgroundColor = "black";
+        td.style.backgroundColor = getRandomColor();
       } else {
         td.style.backgroundColor = "white";
       }
@@ -38,8 +38,7 @@ function generateBoard() {
 
 function makeAlive(x, y) {
   board[x][y].isAlive = true;
-  document.getElementById(`${x}i${y}j`).style.backgroundColor = "black";
-  console.log(board[x][y].isAlive);
+  document.getElementById(`${x}i${y}j`).style.backgroundColor = getRandomColor();
 }
 
 function generateMap() {
@@ -52,67 +51,78 @@ function generateMap() {
   document.getElementById("generateButton").disabled = true;
   generateBoard();
 }
-function chceckState(x, y) {
+function chceckState(x , y) {
+  let checkBoard = board;
   let neighbours = 0;
   //warunki brzegowe
   if (x === 0 && y === 0) {
-    if (board[0][1] === true) this.neighbours = this.neighbours + 1;
-    if (board[1][0] === true) this.neighbours = this.neighbours + 1;
-    if (board[1][1] === true) this.neighbours = this.neighbours + 1;
+    if (this.board[0][1].isAlive === true) neighbours++;
+    if (board[1][0].isAlive === true) neighbours++;
+    if (board[1][1].isAlive === true) neighbours++;
   } else if (x === 0 && y === 24) {
-    if (board[0][23] === true) neighbours = neighbours + 1;
-    if (board[1][24] === true) neighbours = neighbours + 1;
-    if (board[1][23] === true) neighbours = neighbours + 1;
+    if (board[0][23].isAlive === true) neighbours++;
+    if (board[1][24].isAlive === true) neighbours++;
+    if (board[1][23].isAlive === true) neighbours++;
   } else if (x === 24 && y === 0) {
-    if (board[23][0] === true) neighbours = neighbours + 1;
-    if (board[23][1] === true) neighbours = neighbours + 1;
-    if (board[24][1] === true) neighbours = neighbours + 1;
+    if (board[23][0].isAlive === true) neighbours++;
+    if (board[23][1].isAlive === true) neighbours++;
+    if (board[24][1].isAlive === true) neighbours++;
   } else if (x === 24 && y === 24) {
-    if (board[23][23] === true) neighbours = neighbours + 1;
-    if (board[24][23] === true) neighbours = neighbours + 1;
-    if (board[23][24] === true) neighbours = neighbours + 1;
+    if (board[23][23].isAlive === true) neighbours++;
+    if (board[24][23].isAlive === true) neighbours++;
+    if (board[23][24].isAlive === true) neighbours++;
   } else if (x === 0 && y != 0) {
     for (let i = x; i < x + 1; i++) {
       for (let j = y - 1; j < y + 1; j++) {
-        if (board[i][j].isAlive === true) neighbours = neighbours + 1;
+        if(j === y && i === x) continue;
+        if (board[i][j].isAlive === true) neighbours++;
       }
     }
   } else if (x != 0 && y === 0) {
     for (let i = x - 1; i < x + 1; i++) {
       for (let j = y; j < y + 1; j++) {
-        if (board[i][j].isAlive === true) neighbours = neighbours + 1;
+        if(i === x && j === y) continue;
+        if (board[i][j].isAlive === true) neighbours++;
       }
     }
   } else if (x === 24 && y != 0) {
     for (let i = x - 1; i < x; i++) {
       for (let j = y - 1; j < y + 1; j++) {
-        if (board[i][j].isAlive === true) neighbours = neighbours + 1;
+        if(i === x && j === y) continue;
+        if (board[i][j].isAlive === true) neighbours++;
       }
     }
   } else if (x != 0 && y === 24) {
     for (let i = x - 1; i < x + 1; i++) {
       for (let j = y - 1; j < y; j++) {
-        if (board[i][j].isAlive === true) neighbours = neighbours + 1;
+        if(i === x && j === y) continue;
+        if (board[i][j].isAlive === true) neighbours++;
       }
     }
-  } else if (x != 0 && y != 0 && x != 23 && y != 23) {
+  } else /*if (x !== 0 && y !== 0 && x !== 24 && y !== 24)*/ {
     //pozostałe
-    for (let i = x - 1; i < x + 1; i++) {
-      for (let j = y - 1; j < y + 1; j++) {
-        if (board[i][j].isAlive === true) neighbours = neighbours + 1;
+    for (let i = x - 1; i < x + 2; i++) {
+      for (let j = y - 1; j < y + 2; j++) {
+        if(i===x && j === y)
+          continue;
+        if (board[i][j].isAlive === true) neighbours++;
       }
     }
   }
-  if (neighbours === 2 || neighbours === 3) {
-    board[x][y] = true;
-    document.getElementById(`${x}i${y}j`).style.backgroundColor = "black";
-  } else if ((board[x][y] = false && neighbours === 3)) {
-    board[x][y] = true;
-    document.getElementById(`${x}i${y}j`).style.backgroundColor = "black";
-  } else {
-    board[x][y] = false;
+  console.log(neighbours);
+  if (neighbours == 2 || neighbours == 3 && board[x][y].isAlive == true) {
+    document.getElementById(`${x}i${y}j`).style.backgroundColor = getRandomColor();
+  } else if (board[x][y].isAlive === false && neighbours === 3) {
+    checkBoard[x][y].isAlive = true;
+    document.getElementById(`${x}i${y}j`).style.backgroundColor = getRandomColor();
+  } else if (neighbours <= 1 && board[x][y].isAlive === true) {
+    checkBoard[x][y].isAlive = false;
+    document.getElementById(`${x}i${y}j`).style.backgroundColor = "white";
+  } else if (neighbours >= 4 && board[x][y].isAlive === true) {
+    checkBoard[x][y].isAlive = false;
     document.getElementById(`${x}i${y}j`).style.backgroundColor = "white";
   }
+  neighbours = 0;
 }
 function start() {
   for (let i = 0; i < 25; i++) {
@@ -120,18 +130,10 @@ function start() {
       chceckState(i, j);
     }
   }
+  board = checkBoard;
 }
 function startGame() {
-  console.log(board);
-  if (this.gameState === false) {
-    this.gameState = true;
-  } else {
-    this.gameState = false;
-  }
-  while (this.gameState) {
-    setTimeout(start(),5000);
-    this.gameState = false; 
-  }
+  start();
 }
 
 function createLife() {
@@ -140,9 +142,18 @@ function createLife() {
     var a = Math.floor(Math.random() * 25) + 0;
     var b = Math.floor(Math.random() * 25) + 0;
     board[a][b].isAlive = true;
-    document.getElementById(`${a}i${b}j`).style.backgroundColor = "black";
+    document.getElementById(`${a}i${b}j`).style.backgroundColor = getRandomColor();
     loop++;
   }
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 let startBttn = document.createElement("button");
